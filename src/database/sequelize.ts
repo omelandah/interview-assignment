d06c318ef83c5bff777a68f8d3c23d9309bedef1
anhnt276 <ntanh0393@gmail.com>
2025-10-20 14:41:40 +0700

    update sequelize instance on production

--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -1,17 +1,40 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 import modelDefinitions from './models';
+import fs from 'fs';
 
 dotenv.config();
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = process.env;
+const {
+  NODE_ENV,
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_PORT,
+  DB_SSL_CA = '',
+} = process.env;
 
-export const sequelize = new Sequelize(DB_NAME!, DB_USER!, DB_PASSWORD!, {
-  host: DB_HOST,
-  dialect: 'mysql',
-  port: Number(DB_PORT),
-  logging: false,
-});
+const isProduction = NODE_ENV === 'production';
+
+export const sequelize = isProduction
+  ? new Sequelize(DB_NAME!, DB_USER!, DB_PASSWORD!, {
+      host: DB_HOST,
+      dialect: 'mysql',
+      port: Number(DB_PORT),
+      dialectOptions: {
+        ssl: {
+          ca: fs.readFileSync(DB_SSL_CA as string),
+        },
+      },
+      logging: false,
+    })
+  : new Sequelize(DB_NAME!, DB_USER!, DB_PASSWORD!, {
+      host: DB_HOST,
+      dialect: 'mysql',
+      port: Number(DB_PORT),
+      logging: false,
+    });
 
 // Initialize models
 modelDefinitions.forEach((definition) => {
